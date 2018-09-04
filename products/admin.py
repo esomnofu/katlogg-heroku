a135f60a4ba1df5f17d390f9ce52843edda3f0c8
Mr Ofu <mresomnofu@gmail.com>
2018-09-04 11:35:43 +0100

    Adding Codes for Django Admin

--- products/admin.py
+++ products/admin.py
@@ -4,3 +4,5 @@ from .models import Product, News, Ratings
 admin.site.register(Ratings)
 admin.site.register(Product)
 admin.site.register(News)
+admin.site.register(Xpath)
+admin.site.register(Unidentified)
