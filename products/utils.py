96a821c9da0ebe0d2228237665ff32670e451708
Mr Ofu <mresomnofu@gmail.com>
2018-09-07 16:44:39 +0100

    Adding default file as worker

--- products/utils.py
+++ products/utils.py
@@ -2,7 +2,7 @@ import requests
 
 def count_words_at_url(url):
 	print("count_words_at_url request is gotten...")
-    resp = requests.get(url)
+	resp = requests.get(url)
 	print("count_words_at_url response is: ", resp)
 	print("Last response is: ", resp.text.split())
-    return len(resp.text.split())
+	return len(resp.text.split())
